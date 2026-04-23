4eaaeedf1ba71da0f416f277cea0c9eb5d731d77
Joseph Sanchez <joseph.sanchez@iteesa.edu.hn>
2026-04-22 20:40:58 -0600

    Change cerrarTodoElSitio to true

--- guardian.js
+++ guardian.js
@@ -1,7 +1,7 @@
 // ==========================================
 // PANEL DE CONTROL VIP - INFINIHON
 // ==========================================
-const cerrarTodoElSitio = false; 
+const cerrarTodoElSitio = true; 
 const paginasEnObras = ["", ""]; 
 const paginasIntocables = ["fix", "404", "elsanchezok"];
 
